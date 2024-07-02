7130be16a477b71595427a7d9d37c4d6ad34ddfb
EduJobs015 <edualbuq8@gmail.com>
2024-07-01 22:11:20 -0300

    commit 6

--- src/model/EstoquePaes.ts
+++ src/model/EstoquePaes.ts
@@ -5,12 +5,9 @@ export class EstoquePaes{
     precoVenda: number;
         
     constructor(id: number, ModalidadeId: number, quantidade: number,itensComprados: number, precoVenda: number){
-        this.id = this.EstoqueId();
+        this.id = id;
         this.ModalidadeId = ModalidadeId;
         this.quantidade = quantidade;
         this.precoVenda = precoVenda;
-    }
-    private EstoqueId():number{
-        return Date.now();
-        }    
+    }   
 }
---
+++ src/service/ServiceEstoque.ts
@@ -0,0 +1,34 @@
+import {EstoquePaes} from "../model/EstoquePaes";
+import { RepositorioEstoque } from "../repository/RepositorioEstoque";
+
+export class ServiceEstoque{
+    RepositorioEstoque: RepositorioEstoque = new RepositorioEstoque();
+    
+    adicionarEstoque(Produto: any){
+        const{id,ModalidadeId,quantidade,precoVenda} = Produto;
+            if(!id || !ModalidadeId || !quantidade || !precoVenda)
+            throw new Error("Informações incompletas");      
+        const novoProduto = new Produto(id,ModalidadeId,quantidade,precoVenda);
+        this.RepositorioEstoque.adicionarEstoque(novoProduto)
+    }
+    ExibirEstoque():EstoquePaes[]{
+        return this.RepositorioEstoque.ExibirEstoque();
+    }
+    buscarId(id:any):EstoquePaes|undefined{
+        const idService: number = parseInt(id,10);
+            return this.RepositorioEstoque.buscarId(id);     
+    }
+    MudarQuantidade(Produto: any){
+        const{id,ModalidadeId,quantidade,precoVenda} = Produto
+        if(!id || !ModalidadeId || !quantidade || !precoVenda)
+            throw new Error("Informações incompletas");
+        
+
+    }
+
+    
+
+
+
+
+}
